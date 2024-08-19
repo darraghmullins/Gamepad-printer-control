cbbc014f5bbb217cb49f8c76777591b1dac76d58
darraghmullins <darraghmullins@users.noreply.github.com>
2024-08-19 14:07:53 +0100

    Update octocontroller.py
    Updated to suit new robot arm

--- octocontroller.py
+++ octocontroller.py
@@ -12,18 +12,18 @@ except ImportError:
     # !pip3 install pygame
 
 # Define the IP address of your OctoPrint server
-octoprint_ip = "your_ip_here"
+octoprint_ip = "127.0.0.1"
 
 # Define the port number of your OctoPrint server
 octoprint_port = "80"
 
 # Define the API key of your OctoPrint server
-api_key = "your_api_key_here"
+api_key = "BE87410087044812B8F2ED1A99EDE05D"
 
 # Define the button mappings
 button_map = {
-    0: {"command": "jog", "x": 10},  # Move X by 10 to the right
-    1: {"command": "jog", "x": -10},  # Move X by 10 to the left
+    0: {"command": "jog", "z": 1},  # Move Z by 10 to the right
+    1: {"command": "jog", "z": -1},  # Move Z by 10 to the left
     2: {"command": "jog", "y": 10},  # Move Y by 10 to the front
     3: {"command": "jog", "y": -10},  # Move Y by 10 to the back
     4: {"command": "home", "axes": ["x", "y"]},  # Home X and Y axes
@@ -34,12 +34,12 @@ button_map = {
 
 
 # URL for printhead control
-url = "http://{0}:{1}/api/printer/printhead".format(octoprint_ip, octoprint_port)
+url = "http://{0}:{1}/api/printer/".format(octoprint_ip, octoprint_port)
 # Initialize the pygame library
 pygame.init()
 
 # Initialize the video system
-pygame.display.init()
+#pygame.display.init()
 
 # Initialize the joystick
 joystick = pygame.joystick.Joystick(0)
@@ -58,23 +58,89 @@ while True:
     pygame.event.pump()
 
     # Get the X and Y positions of the thumbstick (axes 0 and 1)
-    x = joystick.get_axis(0)
+    z = joystick.get_axis(0)
     y = joystick.get_axis(1)
 
+    x = joystick.get_axis(3)
+    e = joystick.get_axis(4)
+
+    l = joystick.get_axis(2)
+    r = joystick.get_axis(5)
+
+    a = joystick.get_button(0)
+    b = joystick.get_button(1)
+
     # Create a command based on the thumbstick position
-    if x > threshold:
-        command = {"command": "jog", "x": 10}  # Move X by 10 to the right
-    elif x < -threshold:
-        command = {"command": "jog", "x": -10}  # Move X by 10 to the left
+    if z > threshold:
+        command = {"command": "jog", "z": -1}  # Move Z by 10 to the right
+        comp = "printhead"
+    elif z < -threshold:
+        command = {"command": "jog", "z": 1}  # Move Z by 10 to the left
+        comp = "printhead"
     elif y > threshold:
-        command = {"command": "jog", "y": 10}  # Move Y by 10 to the front
+        command = {"command": "jog", "y": 15}  # Move Y by 10 to the front
+        comp = "printhead"
     elif y < -threshold:
-        command = {"command": "jog", "y": -10}  # Move Y by 10 to the back
+        command = {"command": "jog", "y": -15}  # Move Y by 10 to the back
+        comp = "printhead"
+    # else:
+    #     continue  # Skip this iteration if the thumbstick is centered
+
+
+    elif x > threshold:
+        command = {"command": "jog", "x": 1}  # Move Z by 10 to the right
+        comp = "printhead"
+    elif x < -threshold:
+        command = {"command": "jog", "x": -1}  # Move Z by 10 to the left
+        comp = "printhead"
+    elif e > threshold:
+        # command = {"command": "select", "tool": "tool1"}
+        command = {'commands': ['G91', 'M83', 'T0', 'G1 E1 F200', 'M82', 'G90']}  # Move Y by 10 to the front
+        # comp = "tool"
+        comp = "command"
+        # command = {"command": "select", "tool": "tool0","command": "extrude",  "amount": -1}
+    elif e < -threshold:
+        # command = {"command": "select", "tool": "tool1"}
+        command = {'commands': ['G91', 'M83', 'T0', 'G1 E-1 F200', 'M82', 'G90']}  # Move Y by 10 to the back
+        # command = {"command": "select", "tool": "tool0","command": "extrude",  "amount": 1}
+        # comp = "tool"
+        comp = "command"
+        # command = {'command': "M106 S255"}
+
+
+    elif l > 0:
+        # command = {"command": "select", "tool": "tool1"}
+        command = {'commands': ['G91', 'M83', 'T1', 'G1 E1 F100', 'M82', 'G90']}  # Move Y by 10 to the front
+        # comp = "tool"
+        comp = "command"
+        # command = {"command": "select", "tool": "tool1","command": "extrude",  "amount": -1}
+    elif r > 0:
+        # command = {"command": "select", "tool": "tool1"}
+        command = {'commands': ['G91', 'M83', 'T1', 'G1 E-1 F100', 'M82', 'G90']}  # Move Y by 10 to the back
+        # command = {"command": "select", "tool": "tool1", "command": "extrude",  "amount": 1}
+        # comp = "tool"
+        comp = "command"
+        # command = {'command': "M106 S255"}
+
+    elif a:
+        command = {'command': ['M280 P2 S30']}
+        comp = "command"
+    elif b:
+        command = {'command': ['M280 P2 S95']}
+        comp = "command"
+
     else:
         continue  # Skip this iteration if the thumbstick is centered
 
+
+
+
+
     # Send a POST request to the OctoPrint server
-    response = requests.post(url, json=command, headers={'X-Api-Key': api_key})
+    url1 = url + comp
+    response = requests.post(url1, json=command, headers={'X-Api-Key': api_key})
+
+    print(url1, command, api_key)
 
     # Check the response
     if response.status_code == 204:
